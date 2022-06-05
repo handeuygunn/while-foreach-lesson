df98bce8648c9072a3c12a5a42f87f9909362eb3
Hande Nur UYGUN <104138485+handeuygunn@users.noreply.github.com>
2022-06-05 17:28:52 +0300

    Add files via upload

---
+++ donguler-2/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+     static void Main(string[] args)
+    {
+        Console.Write("Lütfen Bir Sayı Giriniz:");
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+
+        while (sayac <= sayi)
+        {
+            toplam += sayac;
+            sayac++;
+        }
+        Console.WriteLine(toplam/sayi);
+
+
+        char character = 'a';
+        while (character <= 'z')
+        {
+            Console.Write(character);
+            character++;
+        }
+        string[] arabalar = {"BMW", "Ford", "Toyoto", "Nissan"};
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+}
